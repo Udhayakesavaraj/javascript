0480f64dce1f4a42118a5380c2a5dc465090e9cc
udhayakesavaraj <udhayakesavarajr.21ece@kongu.edu>
2024-01-22 08:58:48 +0530

    javascript_basics

---
+++ operators.js
@@ -0,0 +1,33 @@
+//Assignment operator
+ a=10
+ console.log("output of a is",a)
+
+ //Arithmetic Operator
+ 
+ var b=10
+ var c=20
+ console.log("addition of b and c is",b+c)
+ console.log("Subtration output:",b-c)
+ console.log("multiplication output:",b*c)
+ console.log("division output:",b/c)
+ 
+ console.log(typeof (b))
+
+ //logical operator &&,||,!
+d=-10
+e=0
+console.log("&& operator output:",d&&e)
+console.log("|| operator outpit:",d||e)
+console.log("! operator outpit:",!d)
+
+
+
+
+
+
+
+
+ 
+ 
+
+
---
+++ variable.js
@@ -0,0 +1,15 @@
+a="a"
+console.log("general variable declaration a=",a)
+var b="var"
+console.log("This variable is var type:",b)
+let c="let"
+console.log("This variable is let type:",c)
+const d=100
+console.log("This variable is const type:",d)
+b=10
+console.log("This variable is var type:",b)
+
+
+
+
+
